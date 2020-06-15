8976bb4ddbf33fcbe9b76e5988a71b8e8aa8b454
azarrias <adolfo.zarrias@gmail.com>
2020-06-15 17:37:18 +0200

    Implement solution for problem 4

--- src/problem_4.py
+++ src/problem_4.py
@@ -38,4 +38,19 @@ def is_user_in_group(user, group):
       user(str): user name/id
       group(class:Group): group to check user membership against
     """
-    return None
+    return user_inheritance(user, group)
+
+def user_inheritance(user, group, rest=[]):
+    if user in group.get_users():
+        return True
+
+    # if there are no other groups to recurse and this group has no children, 
+    # the user is not within the inheritance tree
+    if len(rest) == 0:
+        groups = group.get_groups()
+        if len(groups) == 0:
+            return False
+
+    return user_inheritance(user, groups[0], groups[1:])
+
+print(is_user_in_group("sub_child_user", parent))
